b78c78d8c6a38e83b8c15bdb20df41c48f4ab83f
Komal Batra <KomalBatra@gmail.com>
2018-09-03 13:25:00 -0400

    Final completed version

--- README.md
+++ README.md
@@ -1,13 +1,54 @@
-# Word-Guess-Game
-Homework # 3
+# **Word-Guess-Game**
+### **Creator:** `Komal Batra`
+**Type of Project:** `JavaScript Game`
+
+## Release Notes for Homework #3
 Started on Aug 27th 2018
 
-Welcome to the Word Guess Game also known as Hangman!
+Submitted on Sep 4th 2018
 
-The words to guess are related to Super Heroes!
+- - -
+### **IMPORTANT LINKS**
 
-insert image
+*[Link to the game site](https://komalbatra.github.io/Word-Guess-Game)
 
-Project Drecription
+*[Link to my repository](https://github.com/komalbatra/Word-Guess-Game.git)
 
-Guess
+- - -
+### **Directory Structure**
+
+```
+├── assets
+|  ├── css
+|  |  └── style.css
+|  ├── images
+|  └── javascript
+|     └── game.js
+└── index.html
+```
+
+- - -
+### **Image and Sounds Used**
+
+![First Picture](/assets/images/super.jpg)
+
+*[Win Sound](/assets/images/win2.wav)
+*[Loose Sound](/assets/images/loose.wav)
+
+---
+
+### **How the game works**
+
+1. User is presented with a screen to guess the Super Hero character.
+2. User has 12 tries to find the correct Super Heo name.
+3. Once user presses the first letter, the number of tries aka guesses decrements by 1.
+4. If a user guesses the word, he is presented witha Text saying "You Won" and the number of wins increments by 1.
+5. If a user is unable to guess the word in 12 tries, then the Wins counter does not increment and the user is presented with the text "You Lost".
+
+- - -
+
+### **Known Bugs**
+
+1. Unable to limit the user to enter only letters and numbers
+
+- - -
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,166 +1,89 @@
-// 1. select a few words for hangman
-// 2. Choose a word from the list of words.
-// 3. Prompt the user to enter a key
-// 4. User inputs a key
-// 5. convert that to lower case
-// 6. Set number of GUesses remaining to 12
-// 7. if user does not guess, decrement # of Guesses Remaining by 1...and display the guessed letter as a Captitalized character. 
-// 8. If user guesses the correct letter, display the letter in the guess area. [probably use getElementbyId]
-// 9. If the user does not guess in 12 tries, then display "Try Again" and reset the game with another word.
-// 10. If the user guesses the word, then increment the # of wins by 1. AND display the associated picture/Text/Sound byte. [Can use getElementById for this]
-// 11. Reset the game with another word.
-
-
-/*Word Guess Game Bonuses
-Play a sound or song when the user guesses their word correctly, like in our demo.
-Write some stylish CSS rules to make a design that fits your game's theme.
-HARD MODE: Organize your game code as an object, except for the key events to get the letter guessed. This will be a challenge if you haven't coded with JavaScript before, but we encourage anyone already familiar with the language to try this out.
-Save your whole game and its properties in an object.
-Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
-Don't forget to place your global variables and functions above your object.
-Remember: global variables, then objects, then calls.*/
-
-// Coding ideas:
-// Use https://www.w3schools.com/js/tryit.asp?filename=tryjs_comparison for accepting input and checking against it.
-
-// SuperHero Theme: Superman, Batman, Thor, Aquaman, Spiderman, Hulk, 
-
-//Declarding variable and array//
-var superHeroes = ["superman", "thor", "hulk", "aquaman"]; //selectableWords
+// Declaring Variables
+var superHeroes = ["superman", "thor", "hulk", "batman", "flash"]; 
 
 var currentWord; //the word that needs to be guessed represented by Dashes, when not guessed
 
-var dashes = []; //current word in dashes
+var dashes; //current word in dashes
 
-var wrongLetters = [];
+var wrongLetters;
 
 var remainingGuesses;
 
-var wins;
+var wins = 0;
 
+var letter;
 
+// begining of game that resets everything
 function gameBegin() {
-    currentWord = superHeroes[Math.floor(Math.random() * superHeroes.length)];
     remainingGuesses = 12;
+    dashes = []; 
+    wrongLetters = [];
+    currentWord = superHeroes[Math.floor(Math.random() * superHeroes.length)];
+    
     for (var i = 0; i < currentWord.length; i++) {
-    dashes.push ("_");
+        dashes.push ("_");
     }
-    document.querySelector("#currentWord").innerHTML = dashes.join(" ");
-}
+        document.querySelector("#currentWord").innerHTML = dashes.join(" ");
+        document.querySelector("#remainingGuesses").innerHTML = remainingGuesses;
+        document.querySelector("#wins").innerHTML = wins;
+        document.querySelector("#guessedLetters").innerHTML = "";
+        }
 
-document.onkeyup = function (event) {
-    var letter = String.fromCharCode (event.keyCode).toLowerCase();
-    if (currentWord.indexOf(letter) === -1){ 
-    wrongLetters.push(letter);
-    document.querySelector ("#guessedLetters").innerHTML = wrongLetters.join(" , ");
-    remainingGuesses--;
-    document.querySelector ("#remainingGuesses").innerHTML = remainingGuesses;
-    } else {
-    for (var i = 0; i < currentWord.length; i++) {
-        if (currentWord[i] === letter) {
-            dashes[i] = letter;
-            remainingGuesses--;
-            document.querySelector ("#remainingGuesses").innerHTML = remainingGuesses;
+
+// Game Play function
+function playGame(letter) {
+    if (currentWord.indexOf(letter) === -1){
+        var repeatLetter = false; 
+        for(var k = 0 ; k < wrongLetters.length; k++){
+            if(wrongLetters[k]===letter){
+                repeatLetter = true;
+            }
         }
+            if (!repeatLetter){
+                wrongLetters.push(letter);
+                document.querySelector ("#guessedLetters").innerHTML = wrongLetters.join(" , ");
+                remainingGuesses--;
+        } 
+        
+        document.querySelector ("#remainingGuesses").innerHTML = remainingGuesses;
+        } else {
+        for (var j = 0; j < currentWord.length; j++) {
+            if (currentWord[j] === letter) {
+                dashes[j] = letter;
+                remainingGuesses--;
+                document.querySelector ("#remainingGuesses").innerHTML = remainingGuesses;
+            }
         }
-    document.querySelector ("#currentWord").innerHTML = dashes.join(" ");
+        document.querySelector ("#currentWord").innerHTML = dashes.join(" ");
+        }
     }
-    resultCheck();
-}
 
-
+// Function checks for result
 function resultCheck(){
-if (dashes.indexOf("_") === -1)
-{
-    document.getElementById("correctGuessPic").src = "../assets/images/superman.png"
-}
-else if (remainingGuesses === 0) {
-    // document.querySelector ("#result").innerHTML = ("You Won");
-    Console.log ("You Lost");
-}
-}
-    
-    
-    
+    if (dashes.indexOf("_") === -1)
+    {
+        document.querySelector ("#wins").innerHTML = wins;
+        document.querySelector ("#result").innerHTML = ("You Won");
+        wins++;
+        document.querySelector ("#winMusic").play();
+        gameBegin();
+        
+        }
+    if (remainingGuesses === 0) {
+        document.querySelector ("#result").innerHTML = ("You Lost. Try Again");
+        document.querySelector ("#looseMusic").play();
+        gameBegin();
+        }
+    if ((remainingGuesses >0)&&(remainingGuesses<12)){    
+        document.querySelector ("#result").innerHTML = "";
+    }
+    }
 
+    // key press function
+    document.onkeyup = function (event) {
+        var letter = event.key.toLowerCase();
+        playGame (letter);
+        resultCheck ();
+    };
 
-
-
-
-// NEED TO ADD: TYPEOF (letter)is not a string
-// else if (currentWord.indexOf(letter) > -1){
-//     console.log (letter);
-//     
-
-// }
-
-// // {
-// //     for (var i = 0; i < currentWord.length; i++);{
-//     if (currentWord.[i] === letter) {
-//         dashes[i] = letter;
-//         document.querySelector ("#currentWord").innerHTML = dashes.join (" ");
-       
-//     }   
-//     }
-
-
-
-// }
-    
-
-//     // Captures the key press, converts it to lowercase, and saves it to a variable.
-//     var letter = event.key.toLowerCase();
-//     guessedLetters = currentWord.length;
-//     while (remainGuesses > 0) {
-//     prompt (dashes.join(" "));  
-// }
-// document.onkeyup = function(event) {
-//           userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-//           for (var j = 0; j < currentWord.length; j++){
-//               if (guessedLetters[j] === userGuess) {
-//                   dashes [j] = userGuess;
-//                   numberOfGuesses--;
-//               }
-//               console.log (guessedLetters[j]);
-//           }
-//         }
-// }
-
-
-
-// var guessWord = superHeroes[0]; //Needs to be length of the string...and NEEDS TO LOOP
-
-// var dashes = [];
-// for (var i = 0; i < guessWord.length; i++){
-//     dashes[i] = "_";
-// }
-
-// var remainGuesses = guessWord.length;
-
-// while (remainGuesses > 0) {
-//     console.log (dashes.join(" "));  
-// }
-
-// var guess;
-// document.onkeyup = function(event) {
-//       var guess = String.fromCharCode(event.keyCode).toLowerCase();
-//       for (var j = 0; j < guessWord.length; j++){
-//           if (guessWord[j] === guess) {
-//               dashes [j] = guess;
-//               remainGuesses--;
-//           }
-//       }
-// }
-
-
-// var hangman = ["_", "_", ]
-// var numberGuesses = 12;
-
-// var score = 0;
-
-// function dashes(){}
-
-
-
-// for (var i=0) {
-
+   gameBegin();
